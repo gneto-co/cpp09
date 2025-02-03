570db7093d182b428c5c428ada702d96377f69fc
gneto-co <gneto-co@student.42.fr>
2025-02-03 15:29:46 +0000

    ex02 : done

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,6 +1,6 @@
-#include "../includes/MutantStack.hpp"
+#include "../includes/PmergeMe.hpp"
 
-/* SECTION - press_any_key() */
+// SECTION - press_any_key()
 
 #include <termios.h>
 #include <unistd.h>
@@ -22,105 +22,41 @@ void press_any_key()
 	tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal);
 }
 
-/* !SECTION */
+//! SECTION
 
-/* SECTION - functions */
+// SECTION - main()
 
-template <typename Iterator>
-void print_container(Iterator it_start, Iterator it_end)
+int main(int ac, char **av)
 {
-	while (it_start != it_end)
+	if (ac < 2)
+		return 1;
+
+	MAIN_MSG("vector and deque")
+	try
 	{
-		PRINT << BLUE << *it_start << RESEND;
-		++it_start;
+		PmergeMe<std::vector<int> > pm_vector;
+		PmergeMe<std::deque<int> > pm_deque;
+		//
+		pm_vector.set_time();
+		pm_vector.set_numbers(ac, av);		
+		pm_vector.print_container();
+		pm_vector.sort();
+		double time_vector = pm_vector.get_time();
+		//
+		pm_deque.set_time();
+		pm_deque.set_numbers(ac, av);		
+		pm_deque.sort();
+		double time_deque = pm_deque.get_time();
+		//
+		pm_vector.print_container();
+		PRINT << BRIGHT_BLUE << "(vector) Size: " << pm_vector.get_size() << ", Time: " << time_vector << " seconds" << RESEND;
+		PRINT << BRIGHT_BLUE << "(deque) Size: " << pm_deque.get_size() << ", Time: " << time_deque << " seconds" << RESEND;
 	}
-}
-
-/* !SECTION */
-
-/* SECTION - main() */
-
-int main()
-{
-	MAIN_MSG("Mutant Stack Declaration")
-	MutantStack<int> mt;
-	SPACER(1)
-
-	{
-		MAIN_MSG("top(), size()")
-		mt.push(4);
-		mt.push(2);
-
-		PRINT << BLUE "Top: " << mt.top() << RESEND;
-		PRINT << BLUE "Size: " << mt.size() << RESEND;
-
-		press_any_key();
-		SPACER(1)
-	}
-
-	{
-		MAIN_MSG("pop()")
-		mt.pop();
-
-		PRINT << BLUE "Top: " << mt.top() << RESEND;
-		PRINT << BLUE "Size: " << mt.size() << RESEND;
-
-		press_any_key();
-		SPACER(1)
-	}
-
-	mt.push(2);
-	mt.push(42);
-	mt.push(24);
-	MutantStack<int>::iterator it_begin = mt.begin();
-	MutantStack<int>::iterator it_end = mt.end();
-
-	{
-		MAIN_MSG("print_container() iterator")
-		print_container(it_begin, it_end);
-
-		press_any_key();
-		SPACER(1)
-	}
-
-	{
-		MAIN_MSG("sort() iterator")
-		std::sort(it_begin, it_end);
-		print_container(it_begin, it_end);
-
-		press_any_key();
-		SPACER(1)
-	}
-
-	MutantStack<int>::reverse_iterator rit_begin = mt.rbegin();
-	MutantStack<int>::reverse_iterator rit_end = mt.rend();
-
-	{
-		MAIN_MSG("print_container() reverse_iterator")
-		print_container(rit_begin, rit_end);
-
-		press_any_key();
-		SPACER(1)
-	}
-
-	{
-		MAIN_MSG("sort() reverse_iterator")
-		std::sort(rit_begin, rit_end);
-		print_container(rit_begin, rit_end);
-
-		press_any_key();
-		SPACER(1)
-	}
-
-	{
-		MAIN_MSG("print_container() iterator")
-		print_container(it_begin, it_end);
-
-		press_any_key();
-		SPACER(1)
-	}
+	CATCH
+	
+	SPACER(2)
 
 	return (0);
 }
 
-/* !SECTION */
+//! SECTION
